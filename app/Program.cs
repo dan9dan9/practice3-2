504bc06fd23f2519c045a3f207f5cf7add686494
danil <bd.25.snipermac@mail.ru>
2024-04-03 23:05:27 +0700

    Добавления Main

---
+++ app/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double num1, num2;
+        Console.Write("Введите первое число: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        //Console.WriteLine("Сумма чисел: " + AddNumbers(num1, num2));
+        //Console.WriteLine("Разность чисел: " + SubtractNumbers(num1, num2));
+        //Console.WriteLine("Произведение чисел: " + MultiplyNumbers(num1, num2));
+        //Console.WriteLine("Частное чисел: " + DivideNumbers(num1, num2));
+
+        Console.ReadLine();
+    }
+}
